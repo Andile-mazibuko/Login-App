8981f89442098d6791444324d8a28c1b05e6e0df
Andile <andilelethumusa@gmail.com>
2025-03-14 11:09:11 +0200

    Api functionality completed

--- LogInApp/src/app/app.component.ts
+++ LogInApp/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserApiService } from './services/user-api.service';
+import { UserInterface } from './interfaces/interfaces.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,24 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'LogInApp';
+  existingUsers: UserInterface[] = [];// Array to store fetched users
+
+  //Create an object of our API to retrieve data form it
+  constructor(private userApi: UserApiService){}
+  
+  ngOnInit(): void {
+    this.userApi.getApiData().subscribe(
+      (response) =>{
+        //Assign results to our array
+        this.existingUsers = response;
+        console.log(this.existingUsers);
+      },
+      (error) =>{
+        console.error('Api array not found',error);
+      }
+    );
+  }
+  
 }
